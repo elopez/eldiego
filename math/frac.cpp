944cf2f7c12c01e35e0c0431edef529164139000
mcapretto <margyc94@gmail.com>
2017-11-04 21:43:38 -0300

    comparacion para frac

--- math/frac.cpp
+++ math/frac.cpp
@@ -1,3 +1,20 @@
+bool comp(tipo a, tipo b, tipo c, tipo d){//a*d < b*c
+    int s1 = signo(a)*signo(d), s2 = signo(b)*signo(c);
+    if(s1 == 0) return s2 > 0;
+    if(s2 == 0) return s1 < 0;
+    if(s1 > 0 and s2 < 0) return false;
+    if(s1 < 0 and s2 > 0) return true;
+    if(a / b != c / d){ //asume que b y d son positivos
+	return a / b < c / d;
+    }
+    a %= b, c %= d;
+    /*O(1) pero con double:
+    long double d1 = ((long double)(a))/(b), d2 = ((long double)(c))/(d);
+		return d1 + EPS < d2;
+    */
+    return comp(d, c, b, a);
+}
+
 tipo mcd(tipo a, tipo b){return a?mcd(b%a, a):b;}
 struct frac{
 	tipo p,q;
@@ -19,6 +36,6 @@ struct frac{
 	frac operator/(frac o){
 		tipo a = mcd(q,o.q), b = mcd(o.p,p);
 		return frac((p/b)*(o.q/a),(q/a)*(o.p/b));}
-	bool operator<(const frac &o) const{return p*o.q < o.p*q;}
+	bool operator<(const frac &o) const{return p*o.q < o.p*q;}//usar comp cuando el producto puede dar overflow
 	bool operator==(frac o){return p==o.p&&q==o.q;}
 };
