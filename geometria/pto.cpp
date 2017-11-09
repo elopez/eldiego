d06ccd738a9c1e3498db65a96bb6eca20f4407a6
Fernando Fiori <fefo_f93@hotmail.com>
2017-11-09 14:13:56 -0300

    comments on dot and cross products

--- geometria/pto.cpp
+++ geometria/pto.cpp
@@ -8,14 +8,15 @@ struct pto{
 	pto operator*(double a){return pto(x*a, y*a);}
 	pto operator/(double a){return pto(x/a, y/a);}
 	//dot product, producto interno:
+	//Significado: a*b = a.norm * b.norm * cos(ang).
 	double operator*(pto a){return x*a.x+y*a.y;}
 	//module of the cross product or vectorial product:
-	//if a is less than 180 clockwise from b, a^b>0
+	//if a is less than 180 clockwise from b, a^b>0. Significado: abs(a^b) = area del paralelogramo.
 	double operator^(pto a){return x*a.y-y*a.x;}
 	//returns true if this is at the left side of line qr
 	bool left(pto q, pto r){return ((q-*this)^(r-*this))>0;}
 	bool operator<(const pto &a) const{return x<a.x-EPS || (abs(x-a.x)<EPS && y<a.y-EPS);}
-    bool operator==(pto a){return abs(x-a.x)<EPS && abs(y-a.y)<EPS;}
+	bool operator==(pto a){return abs(x-a.x)<EPS && abs(y-a.y)<EPS;}
 	double norm(){return sqrt(x*x+y*y);}
 	double norm_sq(){return x*x+y*y;}
 };
@@ -33,6 +34,3 @@ pto rotate(pto p, double theta){
 	return pto(p.x*cos(theta)-p.y*sin(theta),
      p.x*sin(theta)+p.y*cos(theta));
 }
-
-
-
