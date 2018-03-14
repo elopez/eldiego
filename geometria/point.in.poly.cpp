664c8d8b466cac6929b0ca567979405c0334f8e2
Fernando Fiori <fefo_f93@hotmail.com>
2018-03-13 23:08:12 -0300

    ahora incluye bordes :)

--- geometria/point.in.poly.cpp
+++ geometria/point.in.poly.cpp
@@ -1,10 +1,14 @@
 //checks if v is inside of P, using ray casting
 //works with convex and concave.
-//excludes boundaries, handle it separately using segment.inside()
+//includes boundaries, handled separately using segment.inside()
 bool inPolygon(pto v, vector<pto>& P) {
 	bool c = false;
 	forn(i, sz(P)){
 		int j=(i+1)%sz(P);
+		
+		//incluye bordes !!!
+		segm lado(P[i],P[j]); if(lado.inside(v)) return true;
+		
 		if((P[j].y>v.y) != (P[i].y > v.y) &&
 	(v.x < (P[i].x - P[j].x) * (v.y-P[j].y) / (P[i].y - P[j].y) + P[j].x))
 			c = !c;
