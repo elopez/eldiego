fbaf8772868b55c7c8bc4d010e2c52ff4b2de21c
Fernando Fiori <fefo_f93@hotmail.com>
2018-04-07 14:09:58 -0300

    comments y blank lines

--- grafos/dfs-paralelo.cpp
+++ grafos/dfs-paralelo.cpp
@@ -1,11 +1,9 @@
 #define MAXN 212345
-
 set<int> G[MAXN];
-
 set<int>::iterator it[MAXN];
 int S[2][MAXN];//pila
-int szS[2];//tamano de la pila
-int szC[2];//tamano de la componente
+int szS[2];//tamaño de la pila
+int szC[2];//tamaño de la componente
 bool vis[MAXN];
 void dfsparalelo(int a, int b){ //O(componente mas chica)
 	zero(vis);
@@ -14,7 +12,6 @@ void dfsparalelo(int a, int b){ //O(componente mas chica)
 	if(sz(G[a])){
 		S[0][szS[0]++] = a;//.push(a);
 		it[a]  = G[a].begin();
-	
 	}
 	if(sz(G[b])){
 		S[1][szS[1]++] = b;//.push(b);
@@ -22,9 +19,8 @@ void dfsparalelo(int a, int b){ //O(componente mas chica)
 	}
 	int act = 0;
 	vis[a] = vis[b] = true;
-
-//recorre las dos componentes en paralelo	
-	while(szS[act]){
+	
+	while(szS[act]){ //recorre las dos componentes en paralelo
 		int v = S[act][szS[act]-1];//.top();
 		int u = *it[v];
 		it[v]++;
@@ -38,10 +34,9 @@ void dfsparalelo(int a, int b){ //O(componente mas chica)
 		}
 		act = 1 - act;
 	}
-	//ya recorrio la toda la componente de act
-	act = 1 - act;
+	act = 1 - act; //ya recorrio la toda la componente de act
 
-	//sigue recorriendo la otra componente hasta que ve un elemento mas o no tiene mas elementos.		
+	//sigue recorriendo la otra componente hasta que ve un elemento más o no tiene más elementos.		
 	while(szC[act] < szC[1-act]+1 and szS[act]){
 		int v = S[act][szS[act]-1];//.top();
 		int u = *it[v];
@@ -53,7 +48,4 @@ void dfsparalelo(int a, int b){ //O(componente mas chica)
 			S[act][szS[act]++] = u;//.push(u);	
 			vis[u] = true;
 			it[u] = G[u].begin();					
-		}
-	}
-	
-}
+}	}	}
