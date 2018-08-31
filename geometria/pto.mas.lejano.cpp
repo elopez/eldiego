b57aea0faed1508e85bffe32298480428ba35cde
Fernando Fiori <fefo_f93@hotmail.com>
2018-08-31 18:35:17 -0300

    comments y orden

--- geometria/pto.mas.lejano.cpp
+++ geometria/pto.mas.lejano.cpp
@@ -4,25 +4,26 @@ int cmp(ll a, ll b){
 	else return -1;
 }
 
-int ternary(int l, int r, pto p){ //estrictamente creciente y luego decreciente en el intervalo (l,r]
-	while(r > l+1){
+int binary(int l, int r, pto p){ //devuelve el índice del punto más lejano en dirección p de (l,r]
+	while(r-l > 1){
 		int m = (l+r)/2;
-		if( p*S[m] > p*S[m+1])
-			r = m;
-		else
-			l = m;
+		if(p*S[m] > p*S[m+1]) r = m;
+		else l = m;
 	}
 	return l+1;
 }
 
+//índice de punto más lejano de S en dirección p. (como tiene dirección puede ser negativa la distancia)
+//o sea, devuelve i si S[i] es el punto con mayor proyección (con signo) sobre la recta que pasa por (0,0) y p.
 int f(pto p, vector<pto>& S){ //S = convex hull
 	ll sz = S.size();
 	if(sz <= 2){
 		ll res = 0;
-		forn(i,sz)
-			if(p * S[i] > p * S[res]) res = i;
+		forn(i,sz) if(p*S[i] > p*S[res]) res = i;
 		return res;	
 	}
+	
+	//busco el S[a] tal que la recta con dirección perpendicular a p que pasa por S[0] divide a S en [1,a] y [a+1,sz-1]
 	ll a = 1, b = sz;
 	while(b-a > 1){
 		ll c = (b+a)/2;
@@ -30,9 +31,8 @@ int f(pto p, vector<pto>& S){ //S = convex hull
 		else a = c;
 	}
 	
-	//la recta con direccion p que pasa por S[0] divide a S en [1,a] y [b,sz-1]
 	if(a == sz-1 and p*S[0] >= p*S[a]) return 0;
-	if(p*S[0] < p*S[a]) return ternary(0,b-1,p);
-	else if(p*S[0] < p*S[b]) return ternary(a,sz-1,p);
+	if(p*S[0] < p*S[a]) return binary(0,a,p);
+	else if(p*S[0] < p*S[b]) return binary(a,sz-1,p);
 	return 0;
 }
