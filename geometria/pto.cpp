a4eff94dc69b1bbbf28250b52c3b6a31c7873c2f
Fernando Fiori <fefo_f93@hotmail.com>
2018-10-13 15:14:00 -0300

    tipo de coordenadas variable

--- geometria/pto.cpp
+++ geometria/pto.cpp
@@ -1,7 +1,8 @@
 const double EPS=1e-9;
+typedef double tipo; //OJO con EPS si es int o ll en < y ==
 struct pto{
-	double x, y;
-	pto(double x=0, double y=0):x(x),y(y){}
+	tipo x, y;
+	pto(tipo x=0, tipo y=0):x(x),y(y){}
 	pto operator+(pto a){return pto(x+a.x, y+a.y);}
 	pto operator-(pto a){return pto(x-a.x, y-a.y);}
 	pto operator+(double a){return pto(x+a, y+a);}
@@ -9,19 +10,19 @@ struct pto{
 	pto operator/(double a){return pto(x/a, y/a);}
 	//dot product, producto interno:
 	//Significado: a*b = a.norm * b.norm * cos(ang).
-	double operator*(pto a){return x*a.x+y*a.y;}
+	tipo operator*(pto a){return x*a.x+y*a.y;}
 	//module of the cross product or vectorial product:
 	//if a is less than 180 clockwise from b, a^b>0. Significado: abs(a^b) = area del paralelogramo.
-	double operator^(pto a){return x*a.y-y*a.x;}
+	tipo operator^(pto a){return x*a.y-y*a.x;}
 	//returns true if this is at the left side of line qr
 	bool left(pto q, pto r){return ((q-*this)^(r-*this))>0;}
 	bool operator<(const pto &a) const{return x<a.x-EPS || (abs(x-a.x)<EPS && y<a.y-EPS);}
 	bool operator==(pto a){return abs(x-a.x)<EPS && abs(y-a.y)<EPS;}
 	double norm(){return sqrt(x*x+y*y);}
-	double norm_sq(){return x*x+y*y;}
+	tipo norm_sq(){return x*x+y*y;}
 };
 double dist(pto a, pto b){return (b-a).norm();}
-double dist_sq(pto a, pto b){return (b-a).norm_sq();}
+tipo dist_sq(pto a, pto b){return (b-a).norm_sq();}
 typedef pto vec;
 
 //positivo si aob están en sentido antihorario con un ángulo <180º
@@ -32,5 +33,4 @@ double angle(pto a, pto o, pto b){ //devuelve radianes! (-pi,pi)
 //rotate p by theta rads CCW w.r.t. origin (0,0)
 pto rotate(pto p, double theta){
 	return pto(p.x*cos(theta)-p.y*sin(theta),
-     p.x*sin(theta)+p.y*cos(theta));
-}
+	p.x*sin(theta)+p.y*cos(theta));}
